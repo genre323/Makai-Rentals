22b0a2c66e97df5943bff1fb2d87181ea6e6fbc8
Gerardo Gonzalez <119821250+genre323@users.noreply.github.com>
2023-04-11 11:37:41 -0700

    Update Reservation.cs

--- .NET-C#/Reservation.cs
+++ .NET-C#/Reservation.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.Reservations
+namespace Makai.Models.Domain.Reservations
 {
     public class Reservation
     {
