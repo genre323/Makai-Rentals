4db22e6887b83f1f60865b550b3a801e218e079f
Gerardo Gonzalez <119821250+genre323@users.noreply.github.com>
2023-04-11 11:38:02 -0700

    Update ReservationAddRequest.cs

--- .NET-C#/ReservationAddRequest.cs
+++ .NET-C#/ReservationAddRequest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Reservations
+namespace Makai.Models.Requests.Reservations
 {
     public class ReservationAddRequest
     {
